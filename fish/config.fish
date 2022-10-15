aad1ae86a47cc3d925e0b921aa7d0ff4c22f66dd
Masashi Tomita <shiganai09@Masashis-MacBook-Air.local>
2022-10-16 02:33:04 +0900

    エイリアス追加　そのうちabbrにする

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,42 @@ if status is-interactive
     set PATH $HOME/go/bin $PATH
     set PATH $HOME/bin $PATH
 end
+
+# cf. https://girigiribauer.com/tech/20200420/
+# git
+alias g "git"
+alias ga "git add"
+alias gaa "git add ."
+alias gb "git branch --all"
+alias gbd "git branch -d "
+alias gc "git commit"
+alias gca "git commit -a"
+alias gco "git checkout"
+alias gcom "git checkout master"
+alias gcod "git checkout develop"
+alias gcob "git checkout -b"
+alias gre "git rebase -i"
+alias gd "git diff"
+alias gl "git log --graph --all --pretty=format:'%Cred%h%Creset %Cgreen(%cI) -%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset' --abbrev-commit --date=rfc2822"
+alias gp "git pull"
+alias gs "git status"
+alias gst "git stash"
+alias gf "git fetch"
+
+# docker
+alias d "docker"
+alias dc "docker container" # override original dc command
+alias dls 'docker container ls'
+alias di "docker image"
+alias dils "docker image ls"
+alias dn "docker network"
+alias dnls "docker network ls"
+alias dv "docker volume"
+alias dvls "docker volume ls"
+alias dcom "docker-compose"
+alias drun "docker run"
+alias dex "docker exec"
+alias dpull "docker pull"
+
+alias fcon "cat ~/.config/fish/config.fish"
+alias fsou "source ~/.config/fish/config.fish"
--- scripts/fish.sh
+++ scripts/fish.sh
@@ -3,15 +3,16 @@
 config_path=$1
 if [ -z "$config_path" ]; then
     config_path=$HOME/.config
+    echo set $config_path
 fi
 
 mkdir -p "$config_path/fish/functions"
 mkdir -p "$config_path/fish/completions"
 mkdir -p "$config_path/fish/themes"
 
-ln -snfv ./fish/config.fish "$config_path/fish"
-ln -snfv ./fish/fish_plugins "$config_path/fish"
-ln -snfv ./fish/fish_variables "$config_path/fish"
-ln -snfv ./fish/functions/* "$config_path/fish/functions"
-ln -snfv ./fish/completions/* "$config_path/fish/completions"
-ln -snfv ./fish/themes/* "$config_path/fish/themes"
+ln -snfv $HOME/dotfiles/fish/config.fish "$config_path/fish"
+ln -snfv $HOME/dotfiles/fish/fish_plugins "$config_path/fish"
+ln -snfv $HOME/dotfiles/fish/fish_variables "$config_path/fish"
+ln -snfv $HOME/dotfiles/fish/functions/* "$config_path/fish/functions"
+ln -snfv $HOME/dotfiles/fish/completions/* "$config_path/fish/completions"
+ln -snfv $HOME/dotfiles/fish/themes/* "$config_path/fish/themes"
