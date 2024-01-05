5b0465da3a0500e1d9983a13defb64ebc31e3b32
tomitamasa <41310325+tomitamasa@users.noreply.github.com>
2024-01-06 03:58:03 +0900

    git用の設定を追加

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -9,6 +9,7 @@ fi
 mkdir -p "$config_path/fish/functions"
 mkdir -p "$config_path/fish/completions"
 mkdir -p "$config_path/fish/themes"
+mkdir -p "$config_path/git"
 
 ln -snfv $HOME/dotfiles/fish/config.fish "$config_path/fish"
 ln -snfv $HOME/dotfiles/fish/fish_plugins "$config_path/fish"
@@ -16,3 +17,5 @@ ln -snfv $HOME/dotfiles/fish/fish_variables "$config_path/fish"
 ln -snfv $HOME/dotfiles/fish/functions/* "$config_path/fish/functions"
 ln -snfv $HOME/dotfiles/fish/completions/* "$config_path/fish/completions"
 ln -snfv $HOME/dotfiles/fish/themes/* "$config_path/fish/themes"
+
+ln -snfv $HOME/dotfiles/git/* $config_path/git
