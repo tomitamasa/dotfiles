b3dd465a1d6fc854480803837804870f97331461
tomitamasa <41310325+tomitamasa@users.noreply.github.com>
2024-01-06 22:43:19 +0900

    fish link fix

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -11,11 +11,9 @@ mkdir -p "$config_path/fish/completions"
 mkdir -p "$config_path/fish/themes"
 mkdir -p "$config_path/git"
 
+ln -snfv $HOME/dotfiles/fish/completions/* "$config_path/fish/completions"
+ln -snfv $HOME/dotfiles/fish/functions/* "$config_path/fish/functions"
 ln -snfv $HOME/dotfiles/fish/config.fish "$config_path/fish"
 ln -snfv $HOME/dotfiles/fish/fish_plugins "$config_path/fish"
-ln -snfv $HOME/dotfiles/fish/fish_variables "$config_path/fish"
-ln -snfv $HOME/dotfiles/fish/functions/* "$config_path/fish/functions"
-ln -snfv $HOME/dotfiles/fish/completions/* "$config_path/fish/completions"
-ln -snfv $HOME/dotfiles/fish/themes/* "$config_path/fish/themes"
 
 ln -snfv $HOME/dotfiles/git/* $config_path/git
