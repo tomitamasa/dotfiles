204010dbd20d3177cb285f5969c8b976be49e36d
tomitamasa <41310325+tomitamasa@users.noreply.github.com>
2025-10-12 20:23:20 +0900

    シンボリックリンク修正

--- fish/completions/ghq.fish
+++ fish/completions/ghq.fish
@@ -1,1 +1,21 @@
-/Users/masashi.tomita/dotfiles/fish/completions/ghq.fish
+# ghq completion for fish shell
+# Based on https://github.com/x-motemen/ghq
+
+complete -c ghq -f
+complete -c ghq -s h -l help -d 'Show help'
+complete -c ghq -s v -l version -d 'Show version'
+complete -c ghq -l silent -d 'Run in silent mode'
+complete -c ghq -l update -d 'Update an existing repository'
+complete -c ghq -l shallow -d 'Do a shallow clone'
+complete -c ghq -l look -d 'Look into a local repository'
+complete -c ghq -l vcs -a 'git svn hg darcs fossil bzr' -d 'Specify VCS backend'
+complete -c ghq -l root -d 'Specify root directory'
+
+# Subcommands
+complete -c ghq -n '__fish_use_subcommand' -a 'get' -d 'Clone/sync with a remote repository'
+complete -c ghq -n '__fish_use_subcommand' -a 'list' -d 'List local repositories'
+complete -c ghq -n '__fish_use_subcommand' -a 'look' -d 'Look into a local repository'
+complete -c ghq -n '__fish_use_subcommand' -a 'root' -d 'Show repositories root'
+
+# Completions for 'ghq look' and 'ghq list'
+complete -c ghq -n '__fish_seen_subcommand_from look list' -a '(ghq list)' -d 'Repository'
--- fish/functions/git_commit_all_verbose.fish
+++ fish/functions/git_commit_all_verbose.fish
@@ -1,1 +1,4 @@
-/Users/masashi.tomita/dotfiles/fish/functions/git_commit_all_verbose.fish
+function git_commit_all_verbose -d "Add all changes and commit with verbose mode"
+    git add -A
+    git commit -v $argv
+end
